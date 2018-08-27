26da05dc186490308f8520b048af3db0a6c5e2d4
BorisIvanovic <borisi@cs.stanford.edu>
2018-08-27 15:25:24 -0700

    Build site at 2018-08-27 22:25:24 UTC

--- _site/js/GreatLeaderSelector.js
+++ _site/js/GreatLeaderSelector.js
@@ -1,6 +1,6 @@
 var MarcoCMYK = ['GreatLeaderC.svg','GreatLeaderM.svg','GreatLeaderY.svg','GreatLeaderK.svg','GreatLeaderR.svg','GreatLeaderG.svg','GreatLeaderB.svg']
 var MarcoSVGimg = MarcoCMYK[Math.floor(Math.random() * MarcoCMYK.length)];
 
-MarcoSrc = 'asl-origin.stanford.edu/img/'+MarcoSVGimg
+MarcoSrc = 'https://stanfordasl.github.io/img/'+MarcoSVGimg
 
 document.getElementById("Chairman").src = MarcoSrc
